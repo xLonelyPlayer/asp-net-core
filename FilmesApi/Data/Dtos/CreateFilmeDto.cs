e276331db75c736a2a0e5e84b3fa144ed5069efd
Augusto Prodohl <51026238+xLonelyPlayer@users.noreply.github.com>
2023-07-18 18:32:34 -0300

    Initial commit

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "Informe um título")]
+    [StringLength(100, ErrorMessage = "Título muito grande")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "Informe um gênero")]
+    [StringLength(50, ErrorMessage = "Gênero muito grande")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 300, ErrorMessage = "O filme deve ter entre 70 e 300 minutos")]
+    public int Duracao { get; set; }
+}
---
+++ FilmesApi/Models/Cinema.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Cinema
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string Nome { get; set; }
+}
